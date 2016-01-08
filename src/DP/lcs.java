ae3e38a8bb6fd270f64944610afc90c040285aba
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-01-07 21:32:13 -0500

    Added alg LSC

---
+++ src/DP/lcs.java
@@ -0,0 +1,12 @@
+public static int lcs(String x, String y, int m, int n) {
+	int[][] L = new int[m+1][n+1];
+
+	for (int i = 0; i <= m; i++) {
+		for (int j = 0; j <= n; j++) {
+			if (i == 0 || j == 0) L[i][j] = 0;
+			else if (x.charAt(i - 1) == y.charAt(j - 1)) L[i][j] = L[i - 1][j - 1] + 1;
+			else L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
+		}
+	}
+	return L[m][n];
+}
